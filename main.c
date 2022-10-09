581e14d44938e0ef8eed6a1dd7665672c3282eae
RyanBean-Code <ryan.bean@wsu.edu>
2022-10-09 16:06:42 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+typedef struct cell     {
+    int alive,                  // 1 means alive 0 means dead
+        generation,             // generation the cell was born into, not sure if needed yet
+        row,                    // row position of the cell
+        col;                    // column position of the cell
+    struct cell neighbors[8];   // array of each other the cells neighbors
+} Cell;
+
+
+int main(int argc, char* argv[])    {
+
+}
